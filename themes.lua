e060d40869d25528a71481d631f82ef9cc71dea3
Xero <azamat.tk.2000@gmail.com>
2026-02-08 02:19:58 +0300

    Add themes data structure

---
+++ themes.lua
@@ -0,0 +1,12 @@
+local themes = {
+    theme1 = {
+        background_color = "#FFFFFF",
+        font_color = "#000000",
+    },
+    theme2 = {
+        background_color = "#000000",
+        font_color = "#FFFFFF",
+    },
+}
+
+return themes
